ab2262d3881cd4f0fd3402e4c158a226d5d84394
Aravinda Baliga B <aravinda.baliga@benelog.com>
2024-11-22 18:23:33 +0100

    adding test case to run generate the sources automatically.

--- src/test/java/XsdGenerationTest.java
+++ src/test/java/XsdGenerationTest.java
@@ -6,6 +6,7 @@ import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -32,5 +33,23 @@ class XsdGenerationTest {
 
         assertTrue(expectedRelationsFile.exists(), "Expected file JsonLdSchemaRelations.json to exist"); // Assert that the file exists
         assertTrue(expectedXSDFile.exists(), "Expected file JsonLdSchemaRelations.json to exist"); // Assert that the file exists
+    }
+
+    @Test
+    void runMavenGenerateSources() throws IOException, InterruptedException {
+        //Maven command to be executed
+        final ProcessBuilder processBuilder = new ProcessBuilder("mvn", "generate-sources");
+        processBuilder.directory(new File(System.getProperty("user.dir"))); // Set the working directory to the project root
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);  // Redirect the output and error streams to the console for debugging purposes
+        processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+        final Process process = processBuilder.start(); //Start the Maven process
+        final boolean completed = process.waitFor(5, TimeUnit.MINUTES); // Wait for the process to complete, with a timeout of 5 minutes
+        final int exitCode = process.exitValue();  // Check if the process completed successfully
+        final File generatedSourcesDir = new File("target/generated-sources"); // To check if directory is created
+
+        assertTrue(completed && exitCode == 0, "Maven generate-sources command failed!");
+        assertTrue(generatedSourcesDir.exists(), "Generated sources directory does not exist!"); // Verify that the generated sources directory exists
+
     }
 }
